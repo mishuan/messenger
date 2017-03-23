6afb2fbdae84b978d5e718c233ef0f644bdcf115
Michael Yuan <michael.sh.yuan@gmail.com>
2017-03-22 21:52:04 -0400

    some android stuff

--- AndroidClient/app/src/main/java/com/ece416/aruproy/messengerclient/LoginActivity.java
+++ AndroidClient/app/src/main/java/com/ece416/aruproy/messengerclient/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.ece416.aruproy.messengerclient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
 
 /**
  * Created by ilikecalculus on 2017-03-22.
@@ -12,5 +16,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    protected void loginOnClick(View v) {
+        Intent i = new Intent(LoginActivity.this, ChatActivity.class);
+        AutoCompleteTextView source = (AutoCompleteTextView) findViewById(R.id.username);
+        i.putExtra(Constants.USERNAME, source.getText().toString());
+        LoginActivity.this.startActivity(i);
     }
 }
