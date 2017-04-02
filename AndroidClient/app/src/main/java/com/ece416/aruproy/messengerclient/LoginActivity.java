2a34f9627d26cb8cdec4227df60e9cc58fe3708e
Michael Yuan <michael.sh.yuan@gmail.com>
2017-04-02 18:21:34 -0400

    asdfasdfa

--- AndroidClient/app/src/main/java/com/ece416/aruproy/messengerclient/LoginActivity.java
+++ AndroidClient/app/src/main/java/com/ece416/aruproy/messengerclient/LoginActivity.java
@@ -17,6 +17,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // hack hack hack
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
     }
